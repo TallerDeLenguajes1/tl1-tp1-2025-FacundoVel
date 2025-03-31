018ed6c7b93b187bd4df3c411657864d5fbfcfb3
Facundo <facundo.vel99@gmail.com>
2025-03-31 20:57:17 -0300

    se realiza Ejercicio 3) hasta el apartado f

--- tp1_1.c
+++ tp1_1.c
@@ -3,7 +3,26 @@
 
 int main() {
 
-    printf("hola mundo");
+    //Ejercicio 3)e. 
+    int variable = 20;
+    int *puntero = &variable;
+
+    //1. El contenido del puntero
+    printf("\nEl contenido del puntero es: %d", *puntero);
+
+    //2. Direccion donde apunta el puntero
+    printf("\nDireccion donde apunta el puntero: %p", puntero);
+
+    //3. Direccion de memoria de variable
+    printf("\nDireccion de la variable: %p", &variable);
+
+    //4. direccion de memoria del puntero
+    printf("\nDireccion del puntero: %p", &puntero);
+
+    //5. Tamaño de memoria utilizado por la variable
+    int bytes = sizeof(variable);
+    printf("\nTamaño de la variable en bytes: %d", bytes);
+
 
     return 0;
 }
